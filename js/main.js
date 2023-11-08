2c41c00cb5ce18060c025481ef39b169336ad4b8
futamu <gnx_0118@outlook.jp>
2023-11-09 00:11:20 +0900

    URL変更再修正

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ function init() {
     gMap = L.map("map"); // 地図の生成
     gMap.setView([lat, lng], zoom); // 緯度経度、ズームレベルを設定する
 
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        // L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
         {
             // 著作権の表示
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
