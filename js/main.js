63b1fdd1bf570533f6dd40975510ca642add4720
futamu <gnx_0118@outlook.jp>
2023-11-15 22:50:29 +0900

    マップ戻し

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ function init() {
     gMap = L.map("map", { doubleClickZoom: false }); // 地図の生成
     gMap.setView([lat, lng], zoom); // 緯度経度、ズームレベルを設定する
 
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    //     {
-    //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //     }
-    // ).addTo(gMap);
-    L.MaptilerLayer({
-        apiKey: 'kLDcubnaeP3o00MMpScj',
-        style: "jp-mierune-streets",
-    }).addTo(gMap);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }
+    ).addTo(gMap);
+    // L.MaptilerLayer({
+    //     apiKey: 'kLDcubnaeP3o00MMpScj',
+    //     style: "jp-mierune-streets",
+    // }).addTo(gMap);
 
     getGeoJson();
 }
