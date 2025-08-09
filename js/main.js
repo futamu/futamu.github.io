3678f874513a9e3b9789206a5a2c529c22af80fc
VHAP01 <vhap01@maidify.uk>
2025-08-09 23:51:37 +0900

    add main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function init() {
 
 function getGeoJson() {
     const Http = new XMLHttpRequest();
-    const url = 'https://pipld6xm54.execute-api.ap-northeast-1.amazonaws.com/default/ekimemoMng/stations';
+    const url = 'data/data.geojson';
     Http.open('GET', url);
     Http.send();
 
@@ -49,7 +49,6 @@ function getGeoJson() {
                         fillOpacity: 1,
                         tags: createTags(feature),
                     })
-                        .on('dblclick', onDblClick)
                         .bindPopup(createBindItem(feature));
                 },
             };
@@ -72,40 +71,6 @@ function getGeoJson() {
 
         }
     }
-}
-
-function onDblClick(e) {
-    const id = e.target.feature.properties.Id;
-    const Http = new XMLHttpRequest();
-    var url = 'https://pipld6xm54.execute-api.ap-northeast-1.amazonaws.com/default/ekimemoMng/';
-
-    switch (e.target.feature.properties.Accessed) {
-        case 'true':
-            e.target.feature.properties.Accessed = 'false';
-            url = url + 'checkOut/' + id;
-            Http.open('GET', url);
-            Http.send();
-
-            break;
-        case 'false':
-            e.target.feature.properties.Accessed = 'true';
-            url = url + 'checkIn/' + id;
-            Http.open('GET', url);
-            Http.send();
-            break;
-        default:
-            break;
-    }
-
-    e.target.setStyle({
-        radius: 8,
-        color: selectColor(e.target.feature),
-        weight: 1,
-        opacity: 1,
-        fillColor: selectFillColor(e.target.feature),
-        fillOpacity: 1,
-        tags: createTags(e.target.feature),
-    });
 }
 
 // function selectColor(feature) {
@@ -157,21 +122,21 @@ function createBindItem(feature) {
 }
 
 function createTags(feature) {
-    const type = feature.properties.Type;
-    const accessed = feature.properties.Accessed;
-    const deleted = feature.properties.Deleted;
+    const type = feature.properties.type;
+    const accessed = feature.properties.isCheckedIn;
+    const deleted = feature.properties.isClosed;
 
     const tags = [];
 
     tags.push(type);
 
-    if (accessed == "true") {
+    if (accessed == true) {
         tags.push("checked");
     } else {
         tags.push("unchecked");
     }
 
-    if (deleted == "true") {
+    if (deleted == true) {
         tags.push("inactive");
     } else {
         tags.push("active");
